695a3edaa4caec2f79822cfae2cd466394d24f88
Prince Ndebele <princendex460@hotmail.com>
2022-01-31 22:51:54 +0100

    fixed package.json unwanted changes and server.js unwanted changes

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 3001
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
+
 app.use(require("./routes/apiRoutes"));
-app.use(require('./routes/htmlRoutes'));
+app.use(require("./routes/htmlRoutes"))
 
-
-app.listen(PORT, function(){
-    console.log(`App lisitening on ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`App lisitening on ${PORT}`);
 });
